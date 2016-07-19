eef6773e457fe11d9d10d4dcf7f73af287a51d28
malinatran <tran.malina@gmail.com>
2016-07-19 09:33:17 -0700

    Add math lib, methods to check if filled

--- spec/tic_tac_toe/core_spec.clj
+++ spec/tic_tac_toe/core_spec.clj
@@ -5,24 +5,40 @@
 (describe "tic-tac-toe board"
           (before-all
             (def board (create-board))
-            (def partial-board (vec [nil nil nil nil nil nil "X" "O" "X"]))
-            (def full-board (vec ["X" "O" "X" "O" "X" "O" "X" "O" "X"])))
+            (def partial-board (vec [nil nil nil
+                                     nil nil nil
+                                     "X" "O" "X"]))
+            (def full-board (vec ["X" "O" "X"
+                                  "O" "X" "X"
+                                  "O" "X" "O"]))
+            (def vertical-board (vec ["X" nil nil
+                                      "X" nil nil
+                                      "X" nil nil]))
+            (def horizontal-board (vec ["O" "O" "O"
+                                        nil nil nil
+                                        nil nil nil]))
+            (def forward-diagonal-board (vec [nil nil "O"
+                                              nil "O" nil
+                                              "O" nil nil]))
+            (def backward-diagonal-board (vec ["X" nil nil
+                                               nil "X" nil
+                                               nil nil "X"])))
 
           (describe "create-board"
                     (it "creates a new board with 9 cells"
                         (should= 9 (count (create-board)))))
 
-          (describe "is-cell-empty?"
+          (describe "cell-empty?"
                     (it "returns true if cell is empty space"
                         (let [cell 2]
-                          (should= true (is-cell-empty? board cell)))))
+                          (should= true (cell-empty? board cell)))))
 
-          (describe "is-board-filled?"
+          (describe "board-filled?"
                     (it "returns true if board does not have empty cells"
-                        (should= true (is-board-filled? full-board)))
+                        (should= true (board-filled? full-board)))
 
                     (it "returns false if board has empty cells"
-                        (should= false (is-board-filled? board))))
+                        (should= false (board-filled? board))))
 
           (describe "get-empty-cells"
                     (it "returns a vector of empty cell numbers"
@@ -35,21 +51,62 @@
                     (it "sets cell with marker"
                         (let [cell 1 marker "X"]
                           (should= [nil "X" nil nil nil nil nil nil nil]
-                                   (mark-cell board cell marker)))))
+                                   (mark-cell board cell marker))))
+
+                    (it "throws an error message if cell is already taken"
+                        (let [cell 1 marker "O"]
+                          (should-throw Error "Cell is already taken."
+                                        (mark-cell full-board cell marker)))))
 
           (describe "clear-cell"
                     (it "sets cell to be nil"
                         (let [cell 3]
-                          (should= ["X" "O" "X" nil "X" "O" "X" "O" "X"]
+                          (should= ["X" "O" "X" nil "X" "X" "O" "X" "O"]
                                    (clear-cell full-board cell)))))
 
-          (describe "is-filled?"
+          (describe "filled-with-marker?"
                     (it "returns true if set of cells is filled with same marker"
                         (let [cells ["X" "X" "X"]
                               marker "X"]
-                          (should= true (is-filled? marker cells))))
+                          (should= true (filled-with-marker? cells marker))))
 
                     (it "returns false if set of cells is not filled with same marker"
                         (let [cells ["X" "O" "X"]
                               marker "X"]
-                          (should= false (is-filled? marker cells))))))
+                          (should= false (filled-with-marker? cells marker)))))
+
+          (describe "row-num"
+                    (it "returns 3 as the square root of a board with a length of 9"
+                        (should= 3 (row-num board)))
+
+                    (it "returns 2 as the square root of a board with a length of 4"
+                        (let [small-board [nil nil nil nil]]
+                        (should= 2 (row-num small-board)))))
+
+          (describe "horizontally-filled?"
+                    (it "returns true if a set of three adjacent horizontal cells are filled"
+                        (let [marker "O"]
+                          (should= true (horizontally-filled? horizontal-board marker)))))
+
+          (describe "vertically-filled?"
+                    (it "returns true if a set of three adjacent vertical cells are filled"
+                        (let [marker "X"]
+                          (should= true (vertically-filled? vertical-board marker)))))
+
+          (describe "forward-diagonal-filled?"
+                    (it "returns true if a set of three"
+                        (let [marker "O"]
+                          (should= true (forward-diagonal-filled? forward-diagonal-board marker))))
+
+                    (it "returns false if a set of three"
+                        (let [marker "O"]
+                          (should= false (forward-diagonal-filled? full-board marker)))))
+
+          (describe "backward-diagonal-filled?"
+                    (it "returns true if backward diagonal has the same marker"
+                        (let [marker "X"]
+                          (should= true (backward-diagonal-filled? backward-diagonal-board marker))))
+
+                    (it "returns false if backward diagonal does not have the same marker"
+                        (let [marker "X"]
+                          (should= false (backward-diagonal-filled? full-board marker))))))
--- src/tic_tac_toe/core.clj
+++ src/tic_tac_toe/core.clj
@@ -1,14 +1,15 @@
-(ns tic-tac-toe.core)
+(ns tic-tac-toe.core
+  (:require [clojure.math.numeric-tower :as math]))
 
 (defn create-board
   []
   (vec (repeat 9 nil)))
 
-(defn is-cell-empty?
+(defn cell-empty?
   [board cell]
   (nil? (get board cell)))
 
-(defn is-board-filled?
+(defn board-filled?
   [board]
   (= -1 (.indexOf board nil)))
 
@@ -19,15 +20,67 @@
 
 (defn mark-cell
   [board cell marker]
-  (if (is-cell-empty? board cell)
-    (assoc board cell marker)))
+  (if (cell-empty? board cell)
+    (assoc board cell marker)
+    (throw (Error. "Cell is already taken."))))
 
 (defn clear-cell
   [board cell]
   (assoc board cell nil))
 
-(defn is-filled?
-  [marker cells]
+(defn filled-with-marker?
+  [cells marker]
   (reduce
     (fn [acc cell]
       (and acc (= marker cell))) true cells))
+
+(defn row-num
+  [board]
+  (math/sqrt (count board)))
+
+(defn horizontally-filled?
+  ([board marker]
+   (horizontally-filled? board marker 0))
+  ([board marker index]
+   (loop [board board
+          marker marker
+          length (row-num board)]
+     (if (filled-with-marker? (subvec board index (+ index length)) marker)
+       true
+       (recur board marker (+ index length))))))
+
+(defn vertically-filled?
+  ([board marker]
+   (vertically-filled? board marker 0))
+  ([board marker index]
+   (loop [board board
+          marker marker
+          length (row-num board)]
+     (when (< index length)
+       (let [first-cell  (get board index)
+             second-cell (get board (+ index length))
+             third-cell  (get board (+ index (* length 2)))]
+         (if (filled-with-marker? (vec [first-cell second-cell third-cell]) marker)
+           true
+           (recur board marker (inc index))))))))
+
+(defn forward-diagonal-filled?
+  [board marker]
+  (let [length (row-num board)
+        index (- length 1)
+        first-cell  (get board index)
+        second-cell (get board (* index 2))
+        third-cell  (get board (* index 3))]
+    (if (filled-with-marker? (vec [first-cell second-cell third-cell]) marker)
+      true
+      false)))
+
+(defn backward-diagonal-filled?
+  [board marker]
+  (let [index 0
+        first-cell  (get board index)
+        second-cell (get board (+ index 4))
+        third-cell  (get board (+ index 8))]
+    (if (filled-with-marker? (vec [first-cell second-cell third-cell]) marker)
+      true
+      false)))
